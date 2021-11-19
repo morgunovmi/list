8b68f5c1d68d67e8225f2f8167213b8c3f1d423a
Max Morgunov <revelmax@pop-os.localdomain>
2021-11-19 20:03:23 +0300

    Changed order of functions in list.cpp

--- src/list.cpp
+++ src/list.cpp
@@ -5,183 +5,6 @@
 
 #include "list.h"
 
-int ListError(List *list) {
-    if (!list) {
-        return ERR_NULL;
-    } 
-
-    if (!list->nodes) {
-        return ERR_NULL_ND;
-    }
-
-    if (list->size > list->capacity) {
-        return ERR_CAP_OVERFL;
-    }
-
-    if (list->size == LST_SIZE_POISON || list->capacity == LST_SIZE_POISON) {
-        return ERR_INV_SIZE;
-    }
-
-    if (list->nodes[0].next != 0 || list->nodes[0].prev != 0) {
-        return ERR_INV_ZERO;
-    }
-    
-    for (size_t i = 1; i < list->capacity; i++) {
-        if (list->nodes[i].next == LST_SIZE_POISON) {
-            return ERR_INV_NEXT;
-        }
-
-        if (list->nodes[i].prev == 0 &&
-            list->tail != list->head && i != list->head) {
-            return ERR_INV_HEAD;
-        }
-
-        if (list->nodes[i].next == 0 && i != list->tail
-            && list->nodes[i].prev != LST_SIZE_POISON) {
-            return ERR_INV_TAIL;
-        }
-
-        if (i == list->head && list->size > 0 && list->nodes[i].prev != 0) {
-            return ERR_INV_HEAD;
-        }
-
-        if (i == list->tail && list->size > 0 && list->nodes[i].next != 0) {
-            return ERR_INV_TAIL;
-        }
-    } 
-
-    return 0;
-}
-
-int writeErrToFile(FILE *file, int err) {
-    int res = 0;
-    switch (err) {
-        case ERR_NULL:
-            res = fprintf(file, "ERR_NULL");
-            break;
-
-        case ERR_INV_POS:
-            res = fprintf(file, "ERR_INV_POS");
-            break;
-            
-        case ERR_NULL_ND:
-            res = fprintf(file, "ERR_NULL_NODES");
-            break;
-
-        case ERR_CAP_OVERFL:
-            res = fprintf(file, "ERR_CAPACITY_OVERFLOW");
-            break;
-            
-        case ERR_INV_SIZE:
-            res = fprintf(file, "ERR_INVALID_SIZE");
-            break;
-
-        case ERR_INV_ZERO:
-            res = fprintf(file, "ERR_INVALID_ZERO_ELEM");
-            break;
-
-        case ERR_INV_HEAD:
-            res = fprintf(file, "ERR_INVALID_HEAD");
-            break;
-
-        case ERR_INV_TAIL:
-            res = fprintf(file, "ERR_INVALID_TAIL");
-            break;
-
-        case ERR_INV_NEXT:
-            res = fprintf(file, "ERR_INVALID_NEXT");
-            break;
-
-        default:
-            res = fprintf(file, "UNDEFINED ERROR");
-    }
-    return res;
-}
-
-int ListDump_(List *list, const char *reason, callInfo info) {
-    assert(list);
-    assert(list->nodes);
-
-    system("mkdir -p dumps\n");
-
-    char dotFileName[64] = "";
-    sprintf(dotFileName, "dumps/dump%zu.dot", list->dumpNum);
-
-    FILE *dotFile = fopen(dotFileName, "w");
-    if (!dotFile) {
-        PRINT_ERROR("Error opening file for dump : %s\n", strerror(errno));
-        return ERR_DOT_FILE_OPN;
-    }
-
-    int errCode = ListError(list);
-
-    fprintf(dotFile, "digraph list{\n"
-                    "{\nrankdir=LR;\n"
-                    "node[shape=plaintext];\nedge[color=white]\n"
-                     "\"List<%s>[%p]\n dumped from %s() at %s (%d)\n\n", 
-                             typeName, (void *)list, info.funcName,
-							 info.file, info.line);
-
-    if (errCode == 0) {
-        fprintf(dotFile, "ok\n");
-
-    } else {
-        fprintf(dotFile, "Error ");
-        writeErrToFile(dotFile, errCode);
-        fprintf(dotFile, "\n");
-    }
-
-    fprintf(dotFile, "Constructed in %s() at %s (%d)\n"
-                             "Dump reason : %s\"\n}\n",
-                             list->ctorCallFuncName,
-							 list->ctorCallFile, list->ctorCallLine, reason);
-
-    fprintf(dotFile, "data [shape=record, label=\"{nodes | head | tail | free | size |"
-            "capacity | isSorted } | { %p | <head> %zu | <tail> %zu | <free> %zu | %zu | %zu | %d }\"];",
-            (void *)list->nodes, list->head, list->tail, list->free, list->size,
-            list->capacity, list->isSorted);
-
-    for (size_t i = 0; i < list->capacity; i++) {
-        fprintf(dotFile, "node%zu [shape=record, label=\"{data\\n%lld |"
-                "index\\n%zu} | next\\n%zu | prev\\n%zu\"];", i, 
-                list->nodes[i].data, i, list->nodes[i].next, list->nodes[i].prev);
-    }
-
-    fprintf(dotFile, "\n{\nedge[color=white];\n");
-    for (size_t i = 0; i < list->capacity - 1; i++) {
-        fprintf(dotFile, "node%zu -> node%zu;\n", i, i+1); 
-    }
-    fprintf(dotFile, "}\n");
-
-    fprintf(dotFile, "data:<head> -> node%zu[color=\"green\", label=\"head\"];\n", list->head);
-    fprintf(dotFile, "data:<tail> -> node%zu[color=\"red\", label=\"tail\"];\n", list->tail);
-    fprintf(dotFile, "data:<free> -> node%zu[color=\"yellow\", label=\"free\"];\n", list->free);
-
-    for (size_t i = 1; i < list->capacity; i++) {
-        fprintf(dotFile, "node%zu -> node%zu[label=\"next\"];\n", i, list->nodes[i].next); 
-
-        if (list->nodes[i].prev != LST_SIZE_POISON) {
-            fprintf(dotFile, "node%zu -> node%zu[label=\"prev\"];\n", i, list->nodes[i].prev); 
-        }
-    }
-
-    fprintf(dotFile, "}\n");
-    if (fclose(dotFile) == -1) {
-        PRINT_ERROR("Error closing dot file : %s\n", strerror(errno));
-        return ERR_FILE_CLS;
-    }
-
-    char command[128] = "";
-    sprintf(command, "dot -Tjpg %s -o dumps/dump%zu.jpg", dotFileName, list->dumpNum);
-    system(command);
-    sprintf(command, "rm %s\n", dotFileName);
-    system(command);
-
-    list->dumpNum++;
-
-    return 0;
-}
-
 int ListCtor_(List *list, size_t capacity, callInfo info) {
     assert(list);
     assert(list->nodes == nullptr);
@@ -525,5 +348,182 @@ int ListRemove(List *list, size_t pos) {
     list->size--;
 
     ASSERT_OK(list);
+    return 0;
+}
+
+int ListError(List *list) {
+    if (!list) {
+        return ERR_NULL;
+    } 
+
+    if (!list->nodes) {
+        return ERR_NULL_ND;
+    }
+
+    if (list->size > list->capacity) {
+        return ERR_CAP_OVERFL;
+    }
+
+    if (list->size == LST_SIZE_POISON || list->capacity == LST_SIZE_POISON) {
+        return ERR_INV_SIZE;
+    }
+
+    if (list->nodes[0].next != 0 || list->nodes[0].prev != 0) {
+        return ERR_INV_ZERO;
+    }
+    
+    for (size_t i = 1; i < list->capacity; i++) {
+        if (list->nodes[i].next == LST_SIZE_POISON) {
+            return ERR_INV_NEXT;
+        }
+
+        if (list->nodes[i].prev == 0 &&
+            list->tail != list->head && i != list->head) {
+            return ERR_INV_HEAD;
+        }
+
+        if (list->nodes[i].next == 0 && i != list->tail
+            && list->nodes[i].prev != LST_SIZE_POISON) {
+            return ERR_INV_TAIL;
+        }
+
+        if (i == list->head && list->size > 0 && list->nodes[i].prev != 0) {
+            return ERR_INV_HEAD;
+        }
+
+        if (i == list->tail && list->size > 0 && list->nodes[i].next != 0) {
+            return ERR_INV_TAIL;
+        }
+    } 
+
+    return 0;
+}
+
+int writeErrToFile(FILE *file, int err) {
+    int res = 0;
+    switch (err) {
+        case ERR_NULL:
+            res = fprintf(file, "ERR_NULL");
+            break;
+
+        case ERR_INV_POS:
+            res = fprintf(file, "ERR_INV_POS");
+            break;
+            
+        case ERR_NULL_ND:
+            res = fprintf(file, "ERR_NULL_NODES");
+            break;
+
+        case ERR_CAP_OVERFL:
+            res = fprintf(file, "ERR_CAPACITY_OVERFLOW");
+            break;
+            
+        case ERR_INV_SIZE:
+            res = fprintf(file, "ERR_INVALID_SIZE");
+            break;
+
+        case ERR_INV_ZERO:
+            res = fprintf(file, "ERR_INVALID_ZERO_ELEM");
+            break;
+
+        case ERR_INV_HEAD:
+            res = fprintf(file, "ERR_INVALID_HEAD");
+            break;
+
+        case ERR_INV_TAIL:
+            res = fprintf(file, "ERR_INVALID_TAIL");
+            break;
+
+        case ERR_INV_NEXT:
+            res = fprintf(file, "ERR_INVALID_NEXT");
+            break;
+
+        default:
+            res = fprintf(file, "UNDEFINED ERROR");
+    }
+    return res;
+}
+
+int ListDump_(List *list, const char *reason, callInfo info) {
+    assert(list);
+    assert(list->nodes);
+
+    system("mkdir -p dumps\n");
+
+    char dotFileName[64] = "";
+    sprintf(dotFileName, "dumps/dump%zu.dot", list->dumpNum);
+
+    FILE *dotFile = fopen(dotFileName, "w");
+    if (!dotFile) {
+        PRINT_ERROR("Error opening file for dump : %s\n", strerror(errno));
+        return ERR_DOT_FILE_OPN;
+    }
+
+    int errCode = ListError(list);
+
+    fprintf(dotFile, "digraph list{\n"
+                    "{\nrankdir=LR;\n"
+                    "node[shape=plaintext];\nedge[color=white]\n"
+                     "\"List<%s>[%p]\n dumped from %s() at %s (%d)\n\n", 
+                             typeName, (void *)list, info.funcName,
+							 info.file, info.line);
+
+    if (errCode == 0) {
+        fprintf(dotFile, "ok\n");
+
+    } else {
+        fprintf(dotFile, "Error ");
+        writeErrToFile(dotFile, errCode);
+        fprintf(dotFile, "\n");
+    }
+
+    fprintf(dotFile, "Constructed in %s() at %s (%d)\n"
+                             "Dump reason : %s\"\n}\n",
+                             list->ctorCallFuncName,
+							 list->ctorCallFile, list->ctorCallLine, reason);
+
+    fprintf(dotFile, "data [shape=record, label=\"{nodes | head | tail | free | size |"
+            "capacity | isSorted } | { %p | <head> %zu | <tail> %zu | <free> %zu | %zu | %zu | %d }\"];",
+            (void *)list->nodes, list->head, list->tail, list->free, list->size,
+            list->capacity, list->isSorted);
+
+    for (size_t i = 0; i < list->capacity; i++) {
+        fprintf(dotFile, "node%zu [shape=record, label=\"{data\\n%lld |"
+                "index\\n%zu} | next\\n%zu | prev\\n%zu\"];", i, 
+                list->nodes[i].data, i, list->nodes[i].next, list->nodes[i].prev);
+    }
+
+    fprintf(dotFile, "\n{\nedge[color=white];\n");
+    for (size_t i = 0; i < list->capacity - 1; i++) {
+        fprintf(dotFile, "node%zu -> node%zu;\n", i, i+1); 
+    }
+    fprintf(dotFile, "}\n");
+
+    fprintf(dotFile, "data:<head> -> node%zu[color=\"green\", label=\"head\"];\n", list->head);
+    fprintf(dotFile, "data:<tail> -> node%zu[color=\"red\", label=\"tail\"];\n", list->tail);
+    fprintf(dotFile, "data:<free> -> node%zu[color=\"yellow\", label=\"free\"];\n", list->free);
+
+    for (size_t i = 1; i < list->capacity; i++) {
+        fprintf(dotFile, "node%zu -> node%zu[label=\"next\"];\n", i, list->nodes[i].next); 
+
+        if (list->nodes[i].prev != LST_SIZE_POISON) {
+            fprintf(dotFile, "node%zu -> node%zu[label=\"prev\"];\n", i, list->nodes[i].prev); 
+        }
+    }
+
+    fprintf(dotFile, "}\n");
+    if (fclose(dotFile) == -1) {
+        PRINT_ERROR("Error closing dot file : %s\n", strerror(errno));
+        return ERR_FILE_CLS;
+    }
+
+    char command[128] = "";
+    sprintf(command, "dot -Tjpg %s -o dumps/dump%zu.jpg", dotFileName, list->dumpNum);
+    system(command);
+    sprintf(command, "rm %s\n", dotFileName);
+    system(command);
+
+    list->dumpNum++;
+
     return 0;
 }
